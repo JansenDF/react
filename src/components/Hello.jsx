fe5ad3a3bb8f2fee734e6dd1063d52ba718eb266
JansenDF <jansen.lima.df@gmail.com>
2022-04-19 09:27:23 -0300

    Extraindo componentes para depois renderizar

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,14 +2,21 @@
 function Hello(props){
     const element = (
         <div>
-            <h1>Hello world, {formatName(props.name, props.lastName)}!</h1>
+            <HelloWorld name={props.name} lastName={props.lastName}/>
         </div>
     );
     function formatName(n, ln) {
         const name = n;
         const lastName = ln;
         return <b>{name} {lastName}</b>
-        }
+    }
+
+    function HelloWorld(props){
+        return (
+            <h1>Hello world, {formatName(props.name, props.lastName)}!</h1>
+        );
+    }
+
     return element;
 }
 export default Hello;
