170846a57fcfc5ef450029f1b524914e712ce498
JansenDF <jansen.lima.df@gmail.com>
2022-04-19 11:06:22 -0300

    Transformando uma função do componente em uma Classe

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,9 +1,24 @@
 
-function Clock(props) {
+// function Clock(props) {
+//     return (
+//       <div>
+//         <h2>São exatamente: {props.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+// }
+// export default Clock;
+
+// Transformando a função acima em uma Classe
+
+import React from "react";
+
+class Clock extends React.Component {
+  render(){
     return (
       <div>
-        <h2>São exatamente: {props.date.toLocaleTimeString()}.</h2>
+        <h2>São exatamente: {this.props.date.toLocaleTimeString()}.</h2>
       </div>
     );
+  }
 }
 export default Clock;
